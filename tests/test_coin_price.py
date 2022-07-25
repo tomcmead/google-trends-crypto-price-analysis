370c63115a35208f9c14067ea163cae372d5ba73
Tom <tomcmead0@gmail.com>
2022-07-25 09:39:57 +0100

    update: Adjust coin price test to align with updated coin price class implementation

--- tests/test_coin_price.py
+++ tests/test_coin_price.py
@@ -1,33 +1,18 @@
 import crypto_trend_analysis.coin_price as CP
 import unittest
 
-class TestCoinInterest(unittest.TestCase):
+class TestCoinPrice(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         print("Coin price testing starting...")
+    
+    def testCoinPrices(self):
+        cp = CP.CoinPrice()
+        coin_prices = cp.coinPrices(['bitcoin','terra-luna'], -100)
 
-    def testCoinPriceDate(self):
-        ci = CP.CoinPrice()
-        coin_price = ci.coinPrice("solana","2022-01-09-10", "2022-01-10-10")
-
-        for i in coin_price:
-            self.assertIsNotNone(i)
-
-        avg_price_increase = ci.avgPriceInc(coin_price)        
-        print("Coin Price Increase: {:.4f}%".format(avg_price_increase))     
-        self.assertIsNotNone(avg_price_increase)
-
-    def testCoinPriceHour(self):
-        ci = CP.CoinPrice()
-        coin_price = ci.coinPrice("solana", None, None, 24)
-
-        for i in coin_price:
-            self.assertIsNotNone(i)
-
-        avg_price_increase = ci.avgPriceInc(coin_price)        
-        print("Coin Price Increase: {:.4f}%".format(avg_price_increase))     
-        self.assertIsNotNone(avg_price_increase)
+        print(f"{coin_prices}")
+        self.assertIsNotNone(coin_prices)
 
 if __name__ == "__main__":
     unittest.main()
