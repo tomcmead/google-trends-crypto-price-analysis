850fd3f3e6c6d40b79d77ba3702b04d90d0ee1a3
Tom <tomcmead0@gmail.com>
2022-07-28 14:52:30 +0100

    feat: Added trending coins function - gets tradable tickers, checks if price increase then if trending on Google

--- crypto_trend_analysis/coin_analysis.py
+++ crypto_trend_analysis/coin_analysis.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import crypto_trend_analysis.coin_price as CP
+import crypto_trend_analysis.coin_interest as CI
+import crypto_trend_analysis.traded_binance_coins as BC
 
 class CoinAnalysis:
+
+    def __init__(self):
+        # object instantiation
+        self.bc = BC.TradedBinanceCoins()
+        self.cp = CP.CoinPrice()
+        self.ci = CI.CoinInterest()
+
     def graphPriceInterest(self, df_price, df_interest):
         df_price.join(df_interest)
 
@@ -21,4 +31,34 @@ class CoinAnalysis:
         plt.get_current_fig_manager().full_screen_toggle()
         plt.close()
 
-    
+        return
+
+    def trendingCoins(self):
+        
+        # get all tradable coins on binance
+        tickers = self.bc.tradableBinanceTickers()
+        coins = self.bc.tickerToNames(tickers)
+        
+        # get list of coin names and coingecko id
+        coin_names = []
+        coin_ids = []
+
+        for coin in coins:
+            coin_names.append(coin['name'])
+            coin_ids.append(coin['id'])
+
+        # get coins with 5% price increase in past hour
+        increasing_coins = self.cp.coinsPriceIncrease(coin_ids, 5)
+
+        # for coins with price increase find if coin is trending
+        coins = []
+        for coin in increasing_coins:
+            df_interest = self.ci.coinInterest(coin, hours=4)
+            try:
+                if self.ci.avgInterestChange(df_interest) > 25:
+                    coins.append(coin['name']) 
+            except Exception as e:
+                print(f"Error: {e.args}")
+        
+        return coins
+            
